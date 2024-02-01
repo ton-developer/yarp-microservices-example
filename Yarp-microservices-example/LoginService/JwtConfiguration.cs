f0e80a38a357a459c59d2c8c791257be20b8d2a7
oscar.jara <oscar.jara@adevinta.com>
2024-02-01 22:35:37 +0100

    remove unused

--- Yarp-microservices-example/LoginService/JwtConfiguration.cs
+++
@@ -1,8 +0,0 @@
-namespace Yarp_microservices_example.LoginService;
-
-public class JwtConfiguration
-{
-    public string Issuer { get; set; }
-    public string Audience { get; set; }
-    public string SecretKey { get; set; }
-}
